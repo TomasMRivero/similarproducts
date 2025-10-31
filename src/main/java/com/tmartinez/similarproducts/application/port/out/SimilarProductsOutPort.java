95dd5de97cb0e82e9a3c61e47c6f249fb1704a08
Tomas Martínez <tomm.mr.98@gmail.com>
2025-10-31 11:53:09 +0100

    create-similar-products-port

--- src/main/java/com/tmartinez/similarproducts/application/port/out/SimilarProductsOutPort.java
+++ src/main/java/com/tmartinez/similarproducts/application/port/out/SimilarProductsOutPort.java
@@ -1,4 +1,10 @@
 package com.tmartinez.similarproducts.application.port.out;
 
+import com.tmartinez.similarproducts.domain.model.Product;
+
+import java.util.List;
+
 public interface SimilarProductsOutPort {
+    List<String> getRelatedProducts(String productId);
+    Product getProductDetails(String productId);
 }
