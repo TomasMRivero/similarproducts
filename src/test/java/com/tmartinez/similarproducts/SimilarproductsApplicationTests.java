717add6de3dc93741fefe7e5be56e8d8a152b5fd
Tomas Martínez <tomm.mr.98@gmail.com>
2025-10-31 10:36:26 +0100

    initial-commit

---
+++ src/test/java/com/tmartinez/similarproducts/SimilarproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmartinez.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
