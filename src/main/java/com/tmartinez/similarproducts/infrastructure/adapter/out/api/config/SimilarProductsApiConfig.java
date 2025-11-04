c54c7068b7b91000d109c331a452b0462c616ed3
Tomas Martínez <tomm.mr.98@gmail.com>
2025-11-04 15:18:30 +0100

    create-separate-rest-template

--- src/main/java/com/tmartinez/similarproducts/infrastructure/adapter/out/api/config/SimilarProductsApiConfig.java
+++ src/main/java/com/tmartinez/similarproducts/infrastructure/adapter/out/api/config/SimilarProductsApiConfig.java
@@ -10,15 +10,33 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class SimilarProductsApiConfig {
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder, @Value("${similarproducts.api.baseurl}") String baseUrl) {
+    @Value("${similarproducts.api.baseurl}")
+    private String baseUrl;
+
+
+    @Bean("restTemplate")
+    public RestTemplate restTemplate() {
         if (baseUrl == null || baseUrl.isEmpty()) {
             throw new IllegalArgumentException("baseUrl cannot be null or empty");
         }
 
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(10000);
+        requestFactory.setConnectTimeout(6000);
         requestFactory.setReadTimeout(6000);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
+        return restTemplate;
+    }
+
+    @Bean("restTemplateRetry")
+    public RestTemplate restTemplateRetry() {
+        if (baseUrl == null || baseUrl.isEmpty()) {
+            throw new IllegalArgumentException("baseUrl cannot be null or empty");
+        }
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(20000);
+        requestFactory.setReadTimeout(30000);
         RestTemplate restTemplate = new RestTemplate(requestFactory);
         restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
         return restTemplate;
